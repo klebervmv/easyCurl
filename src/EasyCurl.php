f87b8502ab641b9d2d5165abec61eee90b1863ac
Kleberton Vilela <klebervmv@gmail.com>
2021-05-12 13:18:30 -0300

    inclusao da prepareCurlOpt() que prepara as options do CURL

--- src/EasyCurl.php
+++ src/EasyCurl.php
@@ -1,170 +1,195 @@
 <?php
 
-    namespace klebervmv;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace klebervmv;
+
+use stdClass;
+
+/**
+ * Description of CurlControle
+ *
+ * @author klebertonvilela
+ */
+class EasyCurl
+{
+    /**
+     * @var false|resource
+     */
+    private $curlInit;
+    /**
+     * @var stdClass
+     */
+    private $curlOpt;
+    /**
+     * @var
+     */
+    private $error;
+    /**
+     * @var
+     */
+    private $httpCode;
+    /**
+     * @var
+     */
+    private $Result;
+    /**
+     * @var string
+     */
+    private $contentType;
 
     /**
-     * Description of EasyCurl
-     *
-     * @author Kleberton Vilela
+     * EasyCurl constructor.
+     * @param string $baseUrl
+     * @param bool $sslVerify
+     * @param string $contentType
      */
-    class EasyCurl
+    public function __construct(string $baseUrl, bool $sslVerify = true, string $contentType = "json")
     {
+        $this->curlOpt = new stdClass();
+        $this->curlOpt->baseUrl = $baseUrl;
+        $this->curlOpt->sslFerify = $sslVerify;
+        $this->contentType = $contentType;
+        $this->resetHeader();
 
-        /**
-         * @var \CurlHandle|false|resource
-         */
-        private $curlInit;
-        /**
-         * @var \stdClass
-         */
-        private $curlOpt;
-        /**
-         * @var
-         */
-        private $error;
-        /**
-         * @var
-         */
-        private $httpCode;
-        /**
-         * @var
-         */
-        private $result;
-        /**
-         * @var string
-         */
-        private $contentType;
-
-        /**
-         * EasyCurl constructor.
-         * @param string $baseUrl
-         * @param bool|bool $sslVerify
-         * @param string|string $contentType
-         */
-        public function __construct(string $baseUrl, bool $sslVerify = true, string $contentType = "json")
-        {
-            $this->curlOpt = new \stdClass();
-            $this->curlOpt->baseUrl = $baseUrl;
-            $this->curlOpt->sslFerify = $sslVerify;
-            $this->contentType = $contentType;
-            $this->resetHeader();
-
-            $this->curlInit = curl_init();
-        }
-
-        /**
-         *
-         * @param string $Method GET,POST,PUT,DELETE
-         * @param string $endPoint /endpoint
-         * @param  $postFields array ou stdClass, as it will be converted into a Json
-         */
-        public function render(string $Method, string $endPoint, $postFields = null): EasyCurl
-        {
-            $this->curlOpt->method = $Method;
-            $this->curlOpt->endPoint = $endPoint;
-            $this->curlOpt->postField = (!empty($postFields)) ? (($this->contentType === "json") ? json_encode($postFields) : $postFields) : null;
-
-            return $this;
-        }
-
-        /**
-         *
-         * @param string $header string for header in "key: value" format
-         * can be called several times as the value entered will be inserted into an array
-         */
-        public function setHeader(string $header): EasyCurl
-        {
-            $this->curlOpt->header[] = $header;
-            return $this;
-        }
-
-        /**
-         * @param string $contentType
-         * @return $this
-         */
-        public function setContentType(string $contentType): EasyCurl
-        {
-            $this->contentType = $contentType;
-            return $this;
-        }
-
-        /**
-         * @return $this
-         */
-        public function resetHeader(): EasyCurl
-        {
-            unset($this->curlOpt->header);
-            $ct = ($this->contentType === "json") ? "application/json" : (($this->contentType === "xml") ? "text/xml;charset=UTF-8" : "");
-            $this->curlOpt->header[] = (!empty($ct)) ? "Content-Type:" . $ct : "";
-
-            return $this;
-        }
-
-        /**
-         * @return $this
-         */
-        public function send(): EasyCurl
-        {
-            $this->curlOpt->baseUrl = (substr($this->curlOpt->baseUrl, -1) === "/") ? substr_replace($this->curlOpt->baseUrl, "", -1, 1) : $this->curlOpt->baseUrl;
-
-            $separator = (substr($this->curlOpt->endPoint, 0, 1) === "/") ? "" : "/";
-            $options = array(
-                CURLOPT_URL => $this->curlOpt->baseUrl . $separator . $this->curlOpt->endPoint,
-                CURLOPT_CUSTOMREQUEST => $this->curlOpt->method,
-                CURLOPT_POSTFIELDS => $this->curlOpt->postField,
-                CURLOPT_HTTPHEADER => $this->curlOpt->header,
-                CURLOPT_SSL_VERIFYPEER => $this->curlOpt->sslFerify,
-                CURLOPT_RETURNTRANSFER => true
-            );
-            curl_setopt_array($this->curlInit, $options);
-
-            $responseData = curl_exec($this->curlInit);
-
-            if (curl_errno($this->curlInit)) {
-                $this->error = curl_error($this->curlInit);
-                return $this;
-            }
-
-            $this->httpCode = curl_getinfo($this->curlInit)['http_code'];
-            $this->result = $responseData;
-
-            if ($this->contentType === "json") {
-                $this->jsDecode();
-            }
-
-            return $this;
-        }
-
-        /**
-         *
-         */
-        function jsDecode(): void
-        {
-            $this->result = json_decode($this->result, true);
-        }
-
-        /**
-         * @return int
-         */
-        function getHttpCode(): int
-        {
-            return $this->httpCode;
-        }
-
-        /**
-         * @return mixed
-         */
-        function getResult()
-        {
-            return $this->result;
-        }
-
-        /**
-         * @return mixed
-         */
-        function getError()
-        {
-            return $this->error;
-        }
-
+        $this->curlInit = curl_init();
     }
+
+    /**
+     *
+     * @param string $Method GET,POST,PUT,DELETE
+     * @param string $endPoint /endpoint
+     * @param  $postFields array ou stdClass, pois será convertido em um Json
+     */
+    public function render(string $Method, string $endPoint, $postFields = null): self
+    {
+        $this->curlOpt->method = $Method;
+        $this->curlOpt->endPoint = $endPoint;
+        $this->curlOpt->postField = (!empty($postFields)) ? (($this->contentType === "json") ? json_encode($postFields) : $postFields) : null;
+
+
+        return $this;
+    }
+
+
+    /**
+     *
+     * @param string $header string para header no formato "chave:valor"
+     * pose ser chamada diversas vezes pois o valor informado será inserido em um array
+     */
+    public function setHeader(string $header): self
+    {
+        $this->curlOpt->header[] = $header;
+        return $this;
+    }
+
+    /**
+     * @param string $contentType
+     * @return $this
+     */
+    public function setContentType(string $contentType): self
+    {
+        $this->contentType = $contentType;
+        return $this;
+    }
+
+    /**
+     * @return $this
+     */
+    public function resetHeader(): self
+    {
+        unset($this->curlOpt->header);
+        $ct = ($this->contentType === "json") ? "application/json" : (($this->contentType === "xml") ? "text/xml;charset=UTF-8" : "");
+        $this->curlOpt->header[] = (!empty($ct)) ? "Content-Type:" . $ct : "";
+
+        return $this;
+    }
+
+    /**
+     * @return $this
+     */
+    public function prepareCurlOpt(): self
+    {
+        $this->curlOpt->baseUrl = (substr($this->curlOpt->baseUrl, -1) === "/") ? substr_replace($this->curlOpt->baseUrl, "", -1, 1) : $this->curlOpt->baseUrl;
+
+        $separator = (substr($this->curlOpt->endPoint, 0, 1) === "/") ? "" : "/";
+        $options = array(
+            CURLOPT_URL => $this->curlOpt->baseUrl . $separator . $this->curlOpt->endPoint,
+            CURLOPT_CUSTOMREQUEST => $this->curlOpt->method,
+            CURLOPT_POSTFIELDS => $this->curlOpt->postField,
+            CURLOPT_HTTPHEADER => $this->curlOpt->header,
+            CURLOPT_SSL_VERIFYPEER => $this->curlOpt->sslFerify,
+            CURLOPT_RETURNTRANSFER => true
+        );
+        curl_setopt_array($this->curlInit, $options);
+        return $this;
+    }
+
+    /**
+     * @return $this
+     */
+    public function send(): self
+    {
+        $this->prepareCurlOpt();
+        $responseData = curl_exec($this->curlInit);
+
+        if (curl_errno($this->curlInit)) {
+            $this->error = curl_error($this->curlInit);
+            return $this;
+        }
+
+        $this->httpCode = curl_getinfo($this->curlInit)['http_code'];
+        $this->Result = $responseData;
+
+        if ($this->contentType === "json") {
+            $this->jsDecode();
+        }
+
+        return $this;
+    }
+
+    /**
+     *
+     */
+    function jsDecode(): void
+    {
+        $this->Result = json_decode($this->Result, true);
+    }
+
+    /**
+     * @return int
+     */
+    function getHttpCode(): int
+    {
+        return $this->httpCode;
+    }
+
+    /**
+     * @return mixed
+     */
+    function getResult()
+    {
+        return $this->Result;
+    }
+
+    /**
+     * @return string
+     */
+    function getError(): string
+    {
+        return $this->error;
+    }
+
+    /**
+     * @return false|resource
+     */
+    public function getCurlInit()
+    {
+        return $this->curlInit;
+    }
+
+}
